729136a745e1653e1638d6c879489c44e3d9fc47
David Ward <spy.sappin.your.sentry@gmail.com>
2023-07-07 18:13:02 -0700

    Add basic quest stuff, basic hero outline

--- Assets/Scripts/Quests/Quest.cs
+++ Assets/Scripts/Quests/Quest.cs
@@ -1,20 +1,94 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
-public class Quest : MonoBehaviour
+public class Quest
 {
+  public enum Difficulty
+  {
+    Easy,
+    Medium,
+    Hard,
+    Insane
+  }
 
+  public enum Theme
+  {
+    Simple,
+    Slay,
+    Steal,
+    Save
+  }
 
-  // Start is called before the first frame update
-  void Start()
+  private Difficulty _difficulty;
+  private int _partySize = 1;
+  private Theme _theme;
+  private int _objectiveQuantity;
+  private int _objectiveStringIndex;
+  private int _currentObjectiveProgress;
+
+  private List<Reward> _rewards;
+
+  private string[] _simpleObjects = new string[] {
+    "mushroom",
+    "",
+    "",
+  };
+
+  private string[] _slayObjects = new string[] {
+    "goblin",
+    "imp",
+    "bat",
+    "skeleton",
+    ""
+  };
+
+  private string[] _stealObjects = new string[] {
+    "Mystical Artifact",
+    "gold coin",
+    "",
+  };
+
+  private string[] _saveObjects = new string[] {
+    "princess",
+    "alchemist",
+    "child",
+    "peasant",
+    ""
+  };
+
+  public Quest()
+  {
+    _difficulty = Difficulty.Easy;
+    _partySize = (int)_difficulty + 1;
+    _theme = GetRandomTheme();
+    GenerateRewards();
+  }
+
+  public Quest(Difficulty difficulty)
+  {
+    _difficulty = difficulty;
+    _partySize = (int)_difficulty + 1;
+    _theme = GetRandomTheme();
+    GenerateRewards();
+  }
+
+  public Quest(Difficulty difficulty, Theme theme)
+  {
+    _difficulty = difficulty;
+    _partySize = (int)_difficulty + 1;
+    _theme = theme;
+    GenerateRewards();
+  }
+
+  public Theme GetRandomTheme()
+  {
+    return (Theme)new System.Random().Next(0, Enum.GetValues(typeof(Theme)).Length);
+  }
+
+  public void GenerateRewards()
   {
 
   }
 
-  // Update is called once per frame
-  void Update()
-  {
-
-  }
 }
--- Assets/Scripts/Quests/QuestManager.cs
+++ Assets/Scripts/Quests/QuestManager.cs
@@ -4,15 +4,69 @@ using UnityEngine;
 
 public class QuestManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+  private Quest _currentQuest;
+  private List<Quest> _completedQuests;
+  private List<Quest> _failedQuests;
+  private List<Quest> _currentQuestOptions;
+  private int _currentMaxQuestOptions = 2;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+
+  public Quest GetCurrentQuest()
+  {
+    return _currentQuest;
+  }
+
+  public void CompleteCurrentQuest()
+  {
+    _completedQuests.Add(_currentQuest);
+    _currentQuest = null;
+  }
+
+  public void FailCurrentQuest()
+  {
+    _failedQuests.Add(_currentQuest);
+    _currentQuest = null;
+  }
+
+  public void ClearQuestOptions()
+  {
+    _currentQuestOptions.Clear();
+
+  }
+
+  public void RegenerateQuestOptions()
+  {
+    ClearQuestOptions();
+    for (int i = 0; i < _currentMaxQuestOptions; i++)
     {
-        
+
     }
+  }
+
+  public void AddQuestOption(Quest.Difficulty difficulty)
+  {
+    _currentQuestOptions.Add(new Quest(difficulty));
+  }
+
+  public Quest GenerateQuest(Quest.Difficulty difficulty)
+  {
+    return new Quest(difficulty);
+  }
+
+  public Quest GenerateQuest(Quest.Difficulty difficulty, Quest.Theme theme)
+  {
+    return new Quest(difficulty, theme);
+  }
 }
